02d8d534cc34ae4c05667d160084dc71f672e1c5
Georgy <pigargeorge@gmail.com>
2023-12-25 17:02:53 +0300

    add span and separate styles

--- src/components/value/Value.tsx
+++ src/components/value/Value.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from '../store/store';
+import { RootState } from '../../store/store';
+import './value.css';
 
 export const Value: FC = () => {
   const { value } = useSelector((state: RootState) => state.counter);
-  return <div>Значение: {value}</div>;
+  return <div className='value'>Значение: <span>{value}</span></div>;
 };
