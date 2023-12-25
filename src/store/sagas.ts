78f5e8a84c0df838e1a93167f96543760244a0f3
Georgy <pigargeorge@gmail.com>
2023-12-25 17:00:28 +0300

    add yield race to each saga, add saga cancellation

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,38 +1,74 @@
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery, race, take } from "redux-saga/effects";
 import { makeOperation } from "../api";
 import {
+  CANCEL_ACTION,
   DECREMENT_ASYNC, DECREMENT_BY_VALUE_ASYNC,
   INCREMENT_ASYNC,
   INCREMENT_BY_VALUE_ASYNC,
   setOperationLoading,
-  setOperationSuccess
+  setOperationSuccess, setStopLoading
 } from './reducers/counter';
 import { DecrementAction, DecrementByValueAction, IncrementAction, IncrementByValueAction } from './Interfaces';
 
 
 function* increment({ payload }: IncrementAction) {
   yield put(setOperationLoading());
-  const res: number = yield call(makeOperation, payload.value, 1)
-  yield put(setOperationSuccess(res));
+  const { result, cancel } = yield race({
+    result: call(makeOperation, payload.value, 1),
+    cancel: take(CANCEL_ACTION),
+  });
+
+  if (result || result === 0) {
+    yield put(setOperationSuccess(result));
+  } else {
+    yield put(setStopLoading());
+    console.log("Operation canceled");
+  }
 }
 
 function* decrement({ payload }: DecrementAction) {
   yield put(setOperationLoading());
-  const res: number = yield call(makeOperation, payload.value, -1)
-  yield put(setOperationSuccess(res));
+  const { result, cancel } = yield race({
+    result: call(makeOperation, payload.value, -1),
+    cancel: take(CANCEL_ACTION),
+  });
+
+  if (result || result === 0) {
+    yield put(setOperationSuccess(result));
+  } else {
+    yield put(setStopLoading());
+    console.log("Operation canceled");
+  }
 }
 
 function* incrementByValue({ payload }: IncrementByValueAction) {
   yield put(setOperationLoading());
-  const res: number = yield call(makeOperation, payload.value, payload.input)
-  console.log(res);
-  yield put(setOperationSuccess(res));
+  const { result, cancel } = yield race({
+    result: call(makeOperation, payload.value, payload.input),
+    cancel: take(CANCEL_ACTION),
+  });
+
+  if (result || result === 0) {
+    yield put(setOperationSuccess(result));
+  } else {
+    yield put(setStopLoading());
+    console.log("Operation canceled");
+  }
 }
 
 function* decrementByValue({ payload }: DecrementByValueAction) {
   yield put(setOperationLoading());
-  const res: number = yield call(makeOperation, payload.value, -payload.input)
-  yield put(setOperationSuccess(res));
+  const { result, cancel } = yield race({
+    result: call(makeOperation, payload.value, -payload.input),
+    cancel: take(CANCEL_ACTION),
+  });
+
+  if (result || result === 0) {
+    yield put(setOperationSuccess(result));
+  } else {
+    yield put(setStopLoading());
+    console.log("Operation canceled");
+  }
 }
 
 export function* rootSaga() {
