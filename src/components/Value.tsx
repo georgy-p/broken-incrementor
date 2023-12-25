ae14bfce93b71583f471719228a9df3628b635fd
Georgy <pigargeorge@gmail.com>
2023-12-25 12:58:46 +0300

    update component type

--- src/components/Value.tsx
+++ src/components/Value.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { FC } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from '../store/store';
 
-export const Value: React.FC = () => {
+export const Value: FC = () => {
   const { value } = useSelector((state: RootState) => state.counter);
   return <div>Значение: {value}</div>;
 };
