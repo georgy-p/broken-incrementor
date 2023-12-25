88a2c35d4f2d1575290c0f08523957fc52a85a15
Georgy <pigargeorge@gmail.com>
2023-12-25 16:54:39 +0300

    add cancelAction and CancelButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Value } from "./components/Value";
+import { Value } from "./components/value/Value";
 import { Button } from "./components/button/Button";
 import "./styles.css";
 import { RootState } from './store/store';
 import {
+    cancelAction,
     decrementAsync,
-    decrementByValueAsync,
     incrementAsync,
-    incrementByValueAsync,
-    setInput
 } from './store/reducers/counter';
 import { FC } from 'react';
-import { Input } from './components/Input/Input';
+import { Input } from './components/input/Input';
 import { Spinner } from './components/spinner/Spinner';
+import { CancelButton } from './components/button/CancelButton';
 
 const App: FC = () => {
   const { value, isLoading } = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch();
   const increment = () => dispatch(incrementAsync(value));
   const decrement = () => dispatch(decrementAsync(value));
+  const cancel = () => dispatch(cancelAction());
 
   return (
     <div className="App">
@@ -29,6 +29,7 @@ const App: FC = () => {
         </div>
         <Input />
         {isLoading && <Spinner />}
+        {isLoading && <CancelButton text="Отменить" onClick={cancel} />}
     </div>
   );
 }
